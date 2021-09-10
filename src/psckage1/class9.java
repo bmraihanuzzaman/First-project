c9955a4e681c61f2276b019810ac1193b484914f
Bm Raihanuzzaman <bmraihanuzzaman@Bms-MacBook-Pro.fios-router.home>
2021-09-10 00:03:09 -0400

    second commit

--- src/psckage1/class9.java
+++ src/psckage1/class9.java
@@ -1,11 +1,12 @@
 package psckage1;
 
 public class class9 {
-    public static void main(String[]args){
-        int number= 10786;
-        if(number==2%){
+    public static void main(String[]args) {
+        int number = 10786;
+        if (number == 2 %) {
             System.out.println("even");
-System.out.println("odd ");
+            System.out.println("odd ");
 
+        }
     }
 }
